f91ce14363b1fa33cbcd28831615bdafcad478c8
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-08 15:18:07 +1200

    referee and tests

--- verification/referee.py
+++ verification/referee.py
@@ -1,49 +1,55 @@
-"""
-CheckiOReferee is a base referee for checking you code.
-    arguments:
-        tests -- the dict contains tests in the specific structure.
-            You can find an example in tests.py.
-        cover_code -- is a wrapper for the user function and additional operations before give data
-            in the user function. You can use some predefined codes from checkio.referee.cover_codes
-        checker -- is replacement for the default checking of an user function result. If given, then
-            instead simple "==" will be using the checker function which return tuple with result
-            (false or true) and some additional info (some message).
-            You can use some predefined codes from checkio.referee.checkers
-        add_allowed_modules -- additional module which will be allowed for your task.
-        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
-        remove_allowed_modules -- close standard library modules, as example "math"
-
-checkio.referee.checkers
-    checkers.float_comparison -- Checking function fabric for check result with float numbers.
-        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
-            digits after coma.
-
-checkio.referee.cover_codes
-    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
-        before user function calling, then using this function. For example: if your test's input
-        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
-    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
-
-"""
-
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
+
+def convert_teleports(teleports_str):
+    """str -> list of tuple(int, int)
+    Convert string describing teleports at list of teleports.
+    teleports_str - string describing teleports map
+    return List of teleports
+    """
+    splitted_str = [tuple(t) for t in teleports_str.split(",")]
+    return [tuple(sorted([int(x), int(y)])) for x, y in splitted_str]
+
+
+def check_route(teleports_str, route):
+    """(str, str) -> (boolean, str, str)
+    Check route for given map of teleports and return sucess or not and right part of route
+    teleports_str - string described teleports map
+    route - string with number of stations
+    Return tuple wtih Success or not and part or full route and message text
+    """
+    start = 1
+    teleports_map = convert_teleports(teleports_str)
+    if not route:
+        return False, '', "Route not started"
+    if route[0] != '1' or route[-1] != '1':
+        return False, '', "Route must started and ended at 1"
+    ch_route = route[0]
+    for i in range(len(route) - 1):
+        try:
+            step = tuple(sorted([int(route[i]), int(route[i + 1])]))
+        except ValueError:
+            return False, ch_route, "Route must contain only digits"
+        if not step in teleports_map:
+            return False, ch_route, "No way from {0} to {1}".format(route[i], route[i + 1])
+        teleports_map.remove(step)
+        ch_route += route[i + 1]
+    for s in range(1, 9):
+        if not str(s) in ch_route:
+            return False, ch_route, "You forget about {0}".format(s)
+    return True, ch_route, "Success"
+
+
+def checker(answer, user_result):
+    result = check_route(answer, user_result)
+    return result[0], (result[1], result[2])
+
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
-        },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
-    ).on_ready)
+        checker=checker).on_ready)
--- verification/tests.py
+++ verification/tests.py
@@ -1,36 +1,83 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
-"""
-
+import random
 
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": "12,23,34,45,56,67,78,81",
+            "answer": "12,23,34,45,56,67,78,81"
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": "12,28,87,71,13,14,34,35,45,46,63,65",
+            "answer": "12,28,87,71,13,14,34,35,45,46,63,65"
+        },
+        {
+            "input": "12,15,16,23,24,28,83,85,86,87,71,74,56",
+            "answer": "12,15,16,23,24,28,83,85,86,87,71,74,56"
+        },
+        {
+            "input": "13,14,23,25,34,35,47,56,58,76,68",
+            "answer": "13,14,23,25,34,35,47,56,58,76,68"
+        },
+        {
+            "input": "12,13,14,15,16,17,18,82,83,84,85,86,87",
+            "answer": "12,13,14,15,16,17,18,82,83,84,85,86,87"
+        },
+        {
+            "input": "12,17,87,86,85,82,65,43,35,46",
+            "answer": "12,17,87,86,85,82,65,43,35,46"
+        },
+        {
+            "input": "13,14,34,32,35,52,37,38,78,16,26",
+            "answer": "13,14,34,32,35,52,37,38,78,16,26"
+        },
+        {
+            "input": "12,23,36,68,85,57,74,41,38,62,25",
+            "answer": "12,23,36,68,85,57,74,41,38,62,25"
+        },
+        {
+            "input": "13,37,78,82,26,64,45,51,75,27,34,36,25,35,17,48,47",
+            "answer": "13,37,78,82,26,64,45,51,75,27,34,36,25,35,17,48,47"
+        },
+        {
+            "input": "18,84,47,76,62,23,35,51,87,65,38,41,75",
+            "answer": "18,84,47,76,62,23,35,51,87,65,38,41,75"
+        },
+        {
+            "input": "18,84,43,36,62,25,57,71,16,56,85",
+            "answer": "18,84,43,36,62,25,57,71,16,56,85"
+        },
+        {
+            "input": "16,64,45,53,38,87,72,21,74,52,15,73",
+            "answer": "16,64,45,53,38,87,72,21,74,52,15,73"
         }
+
+
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+
     ]
 }
+
+
+def generateMap(N):
+    """int -> str
+    return random route from 1 to 1 through all N points
+    Constrains: N > 1
+    Return: String contain route.
+    """
+    route = "1"
+    stations = [str(i) for i in range(2, N + 1)]
+    for _ in range(N - 1):
+        route += stations.pop(random.randrange(0, len(stations)))
+    route += "1"
+    teleports = [str(route[i]) + str(route[i + 1]) for i in range(len(route) - 1)]
+    for _ in range(9):
+        rand_teleport = str(random.randrange(1, N + 1)) + str(random.randrange(1, N + 1))
+        if (rand_teleport[0] != rand_teleport[1] and
+                    rand_teleport not in teleports and rand_teleport[::-1] not in teleports):
+            teleports.append(rand_teleport)
+    return ','.join(teleports)
+
+for _ in range(4):
+    r = generateMap(8)
+    TESTS["Extra"].append({"input": r, "answer": r})
